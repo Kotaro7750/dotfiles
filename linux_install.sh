bf87b42d69421728ea2a896044d8b44e91f994f9
Kotaro7750 <7750koutarou@gmail.com>
2019-04-04 10:29:00 +0900

    make linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -7,6 +7,7 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 echo "DO 'chsh' to change default shell!!"
 
+#neovimのインストールと設定
 sudo apt -y install python3-pip python3-dev
 sudo apt -y install python-pip python-dev
 
@@ -20,6 +21,19 @@ sudo apt -y install clang
 sudo apt -y install libclang-dev
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
+#dockerのインストールと設定
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update 
+sudo apt-get install -y docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo systemctl restart docker
 
 #フォントの設定
 
