543045fbd14b23f9fe03ce727f34fda8ae00481e
Kotaro7750 <7750koutarou@gmail.com>
2020-02-11 15:17:31 +0900

    英字キーボードの設定をドキュメントにした

---
+++ init/keyboard_english.md
@@ -0,0 +1,15 @@
+# 英字キーボードを使えるようにするための設定
+http://jun-networks.hatenablog.com/entry/2019/11/20/160003
+
+* 下のコマンドで英字キーボードを選択 
+
+  ```sh
+  sudo dpkg-reconfigure keyboard-configuration
+  ```
+
+* /usr/share/ibus/component/mozc.xml の<layout>の部分をdefault -> enにする。
+* fcitxのメソッドをmozc、英字キーボードだけにし、双方の配列は英字とする。
+* fcitxの全体設定の入力メソッドのオンオフを使いたいバインド(バインドAとする)にする。
+* mozcの設定のキー設定の選択をfcitxのときとは違うバインド(バインドBとする)にする。これは、mozcの日本語と英語を切り替えるために必要。
+
+以上のように設定すると、ログイン時にバインドBで日本語入力を一度有効にするとバインドAでトグルができる。
--- init/linux_install.sh
+++ init/linux_install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get update
 
 #zsh のインストールと設定
 sudo apt-get -y install zsh
@@ -7,13 +8,15 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 echo "DO 'chsh' to change default shell!!"
 
+#英字キーボード用の設定
+sudo apt-get -y install fcitx fcitx-mozc
+
 #neovimのインストールと設定
 sudo apt -y install python3-pip python3-dev
 sudo apt -y install python-pip python-dev
 
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get -y update
 sudo apt-get -y install neovim
 pip3 install neovim
 pip install neovim
@@ -21,27 +24,15 @@ sudo apt -y install clang
 sudo apt -y install libclang-dev
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
-#dockerのインストールと設定
-sudo apt-get update
+#dockerとdocker-composeのインストール
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update 
 sudo apt-get install -y docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+##sudoなしでdockerを使えるようにする
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo systemctl restart docker
-
-
-git clone https://github.com/longld/peda.git ~/peda
-ln -s ~/dotfiles/gdb/gdbinit ~/.gdbinit
-
-git clone https://github.com/slimm609/checksec.sh ~/checksec
-
-sudo apt-get -y install exiftool
-sudo apt-get -y install strings
-
-sudo apt -y install wireshark
