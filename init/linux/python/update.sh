9bb039dc40066d38e6bd221177687ee4520e3c78
Kotaro7750 <7750koutarou@gmail.com>
2020-03-02 22:18:53 +0900

    update python libraries

---
+++ init/linux/python/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#update requirement
+source ~/nvim-python3/bin/activate
+pip install -r ~/dotfiles/init/linux/python/nvim-python3-requirements.txt
+deactivate
+
+source ~/nvim-python2/bin/activate
+pip install -r ~/dotfiles/init/linux/python/nvim-python2-requirements.txt
+deactivate
