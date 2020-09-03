0d0ed4210cff821c509a49fa9eb33ff9475a2096
Kotaro7750 <7750koutarou@gmail.com>
2020-09-03 23:52:38 +0900

    wsl用に調整

--- init/linux/script/tmux.sh
+++ init/linux/script/tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y libevent-dev libncurses-dev bison
+sudo apt install -y libevent-dev libncurses-dev bison automake
 
 rm -rf ./tmux
 git clone https://github.com/tmux/tmux.git
