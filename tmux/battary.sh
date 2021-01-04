6776030d66f3176c7f511f60ca2b825bf88e1441
Kotaro7750 <7750koutarou@gmail.com>
2021-01-04 16:45:53 +0900

    modify

--- tmux/battary.sh
+++ tmux/battary.sh
@@ -2,12 +2,12 @@
 PERCENT=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk -F':' '{print $2}' | sed -s 's/ //g'| sed -s 's/%//g'` 
 STATE=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk -F':' '{print $2}' | sed -s 's/ //g'| sed -s 's/%//g'` 
 
-if [ $STATE = "charging" ]  || [ $STATE = "fully-charged" ] ; then
+if [ "$STATE" = "charging" ]  || [ "$STATE" = "fully-charged" ] ; then
   echo "#[fg=colour118] ${PERCENT}% #[default]"
   exit
 fi
 
-if [ $PERCENT -ge 20 ] ; then
+if [ "$PERCENT" -ge 20 ] ; then
   echo "#[fg=colour33] ${PERCENT}% #[default]"
   exit
 else
