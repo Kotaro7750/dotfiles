7fd74374b59df07a61795cd05be4d37eabd7375d
Kotaro7750 <7750koutarou@gmail.com>
2022-06-09 21:09:08 +0900

    スニペットを整備

--- nvim/lua/plugin-configs/vim-vsnip.lua
+++ nvim/lua/plugin-configs/vim-vsnip.lua
@@ -1,2 +1,4 @@
 vim.api.nvim_set_keymap('i', '<C-k>', '<Plug>(vsnip-expand-or-jump)', {})
 vim.api.nvim_set_keymap('s', '<C-k>', '<Plug>(vsnip-expand-or-jump)', {})
+
+vim.g.vsnip_snippet_dir = vim.call("expand", "~/.vsnip_snippet")
