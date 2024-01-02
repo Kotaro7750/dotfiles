6c7fccb079679bbf35e3177c05e75aadb551a74f
Kotaro7750 <7750koutarou@gmail.com>
2024-01-02 21:09:04 +0900

    Add: VSCode Codicon to nvim-cmp menu window
    cf. https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-codicons-to-the-menu

--- nvim/lua/plugin-configs/nvim-cmp.lua
+++ nvim/lua/plugin-configs/nvim-cmp.lua
@@ -1,5 +1,33 @@
 local cmp = require('cmp')
 
+local cmp_kinds = {
+  Text = '  ',
+  Method = '  ',
+  Function = '  ',
+  Constructor = '  ',
+  Field = '  ',
+  Variable = '  ',
+  Class = '  ',
+  Interface = '  ',
+  Module = '  ',
+  Property = '  ',
+  Unit = '  ',
+  Value = '  ',
+  Enum = '  ',
+  Keyword = '  ',
+  Snippet = '  ',
+  Color = '  ',
+  File = '  ',
+  Reference = '  ',
+  Folder = '  ',
+  EnumMember = '  ',
+  Constant = '  ',
+  Struct = '  ',
+  Event = '  ',
+  Operator = '  ',
+  TypeParameter = '  ',
+}
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -9,6 +37,12 @@ cmp.setup({
   window = {
     --completion = cmp.config.window.bordered(),
     --documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    format = function(_, vim_item)
+      vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+      return vim_item
+    end,
   },
   mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
