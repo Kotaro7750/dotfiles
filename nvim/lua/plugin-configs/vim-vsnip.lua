80855a6a916d93d0cef6f16d750bea8f9b064dbb
Kotaro7750 <7750koutarou@gmail.com>
2024-01-04 00:17:37 +0900

    Chore: Delete vsnip configuration file

--- nvim/lua/plugin-configs/vim-vsnip.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_set_keymap('i', '<C-k>', '<Plug>(vsnip-expand-or-jump)', {})
-vim.api.nvim_set_keymap('s', '<C-k>', '<Plug>(vsnip-expand-or-jump)', {})
-
-vim.g.vsnip_snippet_dir = vim.call("expand", "~/.vsnip_snippet")
