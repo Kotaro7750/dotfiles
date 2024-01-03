84b1a4051f26d7549f7107cd32f387732ce5ad89
Kotaro7750 <7750koutarou@gmail.com>
2024-01-03 18:52:16 +0900

    Fix: Not parenthes and arguments but only function name are completed in lua_ls
    cf. https://luals.github.io/wiki/settings/#completioncallsnippet

--- nvim/lua/plugin-configs/nvim-lspconfig.lua
+++ nvim/lua/plugin-configs/nvim-lspconfig.lua
@@ -102,6 +102,9 @@ local servers = {
   lua_ls = {
     settings = {
       Lua = {
+        completion = {
+          callSnippet = "Replace"
+        },
         diagnostics = {
           globals = { 'vim' }
         }
