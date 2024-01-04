4bdb6b88e04549ed3586c29230dcc94e48145517
Kotaro7750 <7750koutarou@gmail.com>
2024-01-04 17:30:33 +0900

    Fix: Only one source is displayed in completion window of nvim

--- nvim/lua/plugin-configs/nvim-cmp.lua
+++ nvim/lua/plugin-configs/nvim-cmp.lua
@@ -62,25 +62,13 @@ cmp.setup({
   },
   sources = cmp.config.sources(
     {
-      { name = 'nvim_lsp' },
-    },
-    {
-      { name = 'nvim_lua' },
-    },
-    {
       { name = 'luasnip' },
-    },
-    {
-      { name = 'nvim_lsp_signature_help' }
-    },
-    {
+      { name = 'nvim_lsp' },
+      { name = 'nvim_lsp_signature_help' },
+      { name = 'nvim_lua' },
       { name = 'buffer' },
-    },
-    {
-      { name = 'calc' },
-    },
-    {
       { name = 'path' },
+      { name = 'calc' },
     }
   )
 })
