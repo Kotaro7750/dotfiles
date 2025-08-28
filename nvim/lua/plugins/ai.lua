c4fc745ba2ae330f5f79d4df12922e4fb11cef03
Kotaro7750 <7750koutarou@gmail.com>
2025-08-29 07:54:05 +0900

    Add: Introduce MCPHub and git mcp server

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -27,8 +27,13 @@ return {
     version = false, -- Never set this value to "*"! Never!
     opts = {
       provider = 'copilot',
-      copilot = {
-        model = "claude-3.7-sonnet"
+      behaviour = {
+        enable_cursor_planning_mode = true, -- enable cursor planning mode!
+      },
+      providers = {
+        copilot = {
+          model = "claude-sonnet-4",
+        },
       },
       windows = {
         width = 40,
@@ -40,12 +45,24 @@ return {
       },
       file_selector = {
         provider = "telescope",
-      }
+      },
+      -- -- Below are for MCPHub see. https://github.com/ravitemer/mcphub.nvim?tab=readme-ov-file#avantenvim
+      -- -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        return hub and hub:get_active_servers_prompt() or ""
+      end,
+      -- -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
     },
     build = "make",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
+      -- "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
@@ -72,5 +89,40 @@ return {
         },
       },
     },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    cmd = "MCPHub",
+    build = "bundled_build.lua",
+    config = function()
+      require("mcphub").setup({
+        config = vim.fn.expand("~/dotfiles/nvim/mcphub_config.json"),
+        shutdown_delay = 5000, -- milliseconds
+        extensions = {
+          avante = {}
+        },
+        use_bundled_binary = true,
+
+        global_env = function(context)
+          local env = {
+            MCPHUB_PROJECT_ROOT = require("lspconfig.util").root_pattern(".git")(vim.fn.expand("%:p")) or vim.fn.getcwd()
+          }
+          -- Add context-aware variables
+          if context.is_workspace_mode then
+            env.MCPHUB_PROJECT_ROOT = context.workspace_root
+          end
+          return env
+        end,
+
+        workspace = {
+          enabled = true,
+          look_for = { ".mcphub/servers.json", ".vscode/mcp.json", ".cursor/mcp.json" },
+          reload_on_dir_changed = true,
+        }
+      })
+    end,
   }
 }
