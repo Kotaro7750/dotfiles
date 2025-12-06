7232178de8495d5d5f6a87fa9052c0e1eae0fdba
Kotaro7750 <7750koutarou@gmail.com>
2025-12-06 13:51:49 +0900

    Add: Install toggleterm

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -21,5 +21,29 @@ return {
   {
     "kylechui/nvim-surround",
     config = true,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      -- Setup key binding for lazygit
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<Leader>tt', ':ToggleTerm<CR>', { silent = true, noremap = true })
+    end,
+    opts = {
+      persist_mode = false, -- This is for entering insert mode when next toggle
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+    }
   }
 }
