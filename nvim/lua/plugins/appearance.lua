2f0e5699701369a9781fc2b54dee76395659f288
Kotaro7750 <7750koutarou@gmail.com>
2026-01-18 16:08:27 +0900

    Fix: Migrate nvim-treesitter to main branch

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -53,33 +53,21 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = "all",
-      sync_install = false,
-      highlight = {
-        enable = true,
+    config = function(_, _)
+      require("nvim-treesitter").setup({})
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()";
 
-        -- Disable vim built-in highlighting
-        additional_vim_regex_highlighting = false
-      },
-
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
-
-      indent = {
-        enable = false,
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      -- cf. https://blog.atusy.net/2025/08/10/nvim-treesitter-main-branch/
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+        callback = function(ctx)
+          -- 必要に応じて`ctx.match`に入っているファイルタイプの値に応じて挙動を制御
+          -- `pcall`でエラーを無視することでパーサーやクエリがあるか気にしなくてすむ
+          pcall(vim.treesitter.start)
+        end,
+      });
     end
   },
   -- Render markdown
